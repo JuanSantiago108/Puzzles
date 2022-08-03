0e67befd34203167235d8c8e8db08e812a1b3b64
JuanSantiago108 <santiagojuanandres@gmail.com>
2022-08-02 21:38:39 -0400

    sencond commit

--- Program.cs
+++ Program.cs
@@ -41,3 +41,23 @@ static string TossCoin()
     }
 }
 Console.WriteLine(TossCoin());
+
+static List<string> Names()
+{
+    List<string> nameList = new List<string>(){ "Todd", "Tiffany", "Charlie", "Geneva", "Sydney"};
+    List<string> newList = new List<string>(){};
+    foreach (string name in nameList)
+    {
+        if (name.Length > 5)
+        {
+            newList.Add(name);
+        }
+    }
+    return newList;
+}
+List<string> resultList = Names();
+
+foreach (string name in resultList)
+{
+    Console.WriteLine(name);
+}
